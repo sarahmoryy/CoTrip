73dd6fe462913f0e84bc86fcf65422e62cddd171
NoahYe123 <jian.ye@mail.mcgill.ca>
2025-09-16 23:26:59 -0400

    upgrade firebase SDK

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 // import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,8 +21,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app,{
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 // const analytics = getAnalytics(app);
+export const db = getFirestore(app);
